1e3386d8796794543618df07b6bb4559956109e8
Diego Mendes <diego@jaya.tech>
2020-04-29 12:54:14 -0300

    corrrecao regex

--- src/main/java/com/datasus/utils/ProcessSqlUtils.java
+++ src/main/java/com/datasus/utils/ProcessSqlUtils.java
@@ -2,18 +2,19 @@ package com.datasus.utils;
 
 public class ProcessSqlUtils {
 
-    public static String processBeforeInsert(String comandSql) {
+  public static String processBeforeInsert(String comandSql) {
 
-        //corrigindo bug das virgulas
-        comandSql = comandSql.replaceAll("([A-Z])('')", "$1'");
+    //corrigindo bug das virgulas
+    comandSql = comandSql.replaceAll("([a-zA-Z]|[0-9])(')([a-zA-Z]|[0-9])", "$1$3");
+    comandSql = comandSql.replaceAll("([A-Z])('')", "$1'");
 
-        comandSql = comandSql.replaceAll("''''", "''");
-        comandSql = comandSql.replaceAll("'''", "''");
-        comandSql = comandSql.replaceAll("([0-9])('')", "$1'");
-        comandSql = comandSql.replaceAll("('')([0-9])", "'$2");
-        comandSql = comandSql.replaceAll("'='", "'");
-        comandSql = comandSql.replaceAll("([0-9])(')([A-Z])(')", "$1$3$4");
+    comandSql = comandSql.replaceAll("''''", "''");
+    comandSql = comandSql.replaceAll("'''", "''");
+    comandSql = comandSql.replaceAll("([0-9])('')", "$1'");
+    comandSql = comandSql.replaceAll("('')([0-9])", "'$2");
+    comandSql = comandSql.replaceAll("'='", "'");
+    comandSql = comandSql.replaceAll("([0-9])(')([A-Z])(')", "$1$3$4");
 
-        return comandSql;
-    }
+    return comandSql;
+  }
 }
